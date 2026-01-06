2b8adc0eec0b3cf0edb8a7414008538060be28b9
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-06 23:14:39 +0700

    feat: create header and adding it to layout

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
