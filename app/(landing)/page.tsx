c8131754baf04c642370bc4c2d4ab9f5b1ff2332
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-06 23:19:11 +0700

    feat: create categories section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/home/hero";
+import CategorySection from "./components/home/categories";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <CategorySection />
     </main>
   );
 }
