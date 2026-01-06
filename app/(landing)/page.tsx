3304fea542c833670e323cc6a8be7cd8847ccb38
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-06 23:33:27 +0700

    feat: create product section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/home/hero";
 import CategorySection from "./components/home/categories";
+import ProductSection from "./components/home/products";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <CategorySection />
+      <ProductSection />
     </main>
   );
 }
