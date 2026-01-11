03591cb704baf249fc766069f4a1a75c16f6f0a0
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-11 12:30:29 +0700

    refactor: change code styling based on editorconfig file

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,27 +1,27 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'SportOn',
-    short_name: 'SportOn',
-    description: 'SportOn - Your ultimate sports companion',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#ffffff',
+    name: "SportOn",
+    short_name: "SportOn",
+    description: "SportOn - Your ultimate sports companion",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
     icons: [
       {
-        src: '/icons/web-app-manifest-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'maskable',
+        src: "/icons/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
-        src: '/icons/web-app-manifest-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
+        src: "/icons/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
-  }
+  };
 }
