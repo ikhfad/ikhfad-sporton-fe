326f9a9c7a8de52e3540f58f74b5b6802c46e925
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-11 18:02:25 +0700

    style: make order status main section full screen

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -8,7 +8,7 @@ const OrderStatus = () => {
   const [isConfirmed, setIsConfirmed] = useState(false);
 
   return (
-    <main className="bg-gray-100 min-h-[80vh]">
+    <main className="bg-gray-100 h-screen">
       <div className="max-w-5xl mx-auto py-20">
         <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
       </div>
