8f70c06cdc2b244c79141a24fb471f75517c2bb3
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-14 22:38:42 +0700

    feat: implement dynamic bank data fetching and transaction checkout

--- app/(landing)/components/payment/payment-options.tsx
+++ app/(landing)/components/payment/payment-options.tsx
@@ -1,36 +1,24 @@
 import { FiCreditCard } from "react-icons/fi";
 import CardWithHeader from "../ui/card-with-header";
+import { getAllBank } from "@/app/services/bank.service";
 
-const paymentsList = [
-  {
-    bank_name: "BCA",
-    account_number: 8792149719496,
-    account_holder: "PT SportOn Digital",
-  },
-  {
-    bank_name: "BRI",
-    account_number: 7659791874917,
-    account_holder: "PT SportOn Digital",
-  },
-  {
-    bank_name: "Mandiri",
-    account_number: 9462875629486,
-    account_holder: "PT SportOn Digital",
-  },
-];
+const PaymentOptions = async () => {
+  const banks = await getAllBank();
 
-const PaymentOptions = () => {
   return (
     <CardWithHeader title="Payment Options">
-      {paymentsList.map((payment, index) => (
-        <div className="flex gap-5 p-5 border-b border-gray-100" key={index}>
+      {banks.map((payment) => (
+        <div
+          className="flex gap-5 p-5 border-b border-gray-100"
+          key={payment._id}
+        >
           <div className="bg-blue-100 p-4 text-blue-500 flex justify-center items-center">
             <FiCreditCard size={24} />
           </div>
           <div className="self-center">
-            <div className="font-bold">{payment.bank_name}</div>
-            <div className="text-sm">{payment.account_number}</div>
-            <div className="text-sm opacity-70">{payment.account_holder}</div>
+            <div className="font-bold">{payment.bankName}</div>
+            <div className="text-sm">{payment.accountNumber}</div>
+            <div className="text-sm opacity-70">{payment.accountName}</div>
           </div>
           <div className="ml-auto bg-blue-50 text-gray-800 text-xs h-fit self-center px-2 py-1">
             Bank Transfer
