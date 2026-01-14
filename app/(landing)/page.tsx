81c82ea4016ac5e4475fede0dd9493ddc724ca55
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-14 09:02:26 +0700

    feat: remove category list mockup data and connect categories section to live api

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/home/hero";
 import CategorySection from "./components/home/categories";
 import ProductSection from "./components/home/products";
+import { getAllCategories } from "../services/category.service";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await getAllCategories();
   return (
     <main>
       <HeroSection />
-      <CategorySection />
+      <CategorySection categories={categories} />
       <ProductSection />
     </main>
   );
