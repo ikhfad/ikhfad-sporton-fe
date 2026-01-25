38b3b73e0b117f14124a87258520fa9e2c031390
ikhfad <59162321+ikhfad@users.noreply.github.com>
2026-01-25 16:12:43 +0700

    fix: handle empty path in getImageUrl function

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -35,6 +35,7 @@ export async function fetchAPI<T>(
 }
 
 export function getImageUrl(path: string) {
+  if (!path) return "";
   if (path.startsWith("http")) return path;
   return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`;
 }
